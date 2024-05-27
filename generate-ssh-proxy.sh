6b577814b4e28a11bb1959278c8005946f04e761
Pape Samba NDOUR <papesambandour@hotmail.com>
2024-05-27 20:47:24 +0000

    init repo

--- generate-ssh-proxy.sh
+++ generate-ssh-proxy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# Colors for output formatting
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-#config
 FILE="$SCRIPT_DIR/thycotic.config"
 validate_input() {
     if [ -z "$1" ]; then
@@ -13,17 +11,14 @@ validate_input() {
         exit 1
     fi
 }
-# Check if the file exists
+
 if [ ! -f "$FILE" ] || [ "$1" = "login" ]; then
-#    echo -e "${YELLOW}Le fichier $FILE n'existe pas. Merci de saisir vos accès thycotic."
     echo -e "${GREEN}Configuration des accès Thycotic${YELLOW}"
-    # Prompt the user for values
+
     read -rp "Entrer thycotic_username: " thycotic_username
     read -srp "Entrer thycotic_password: " thycotic_password
-    echo # Move to a new line after password input
     read -rp "Entrer thycotic_sub_domaine: " thycotic_sub_domaine
 
-    # Create the file and write the values to it
     echo "thycotic_username=$thycotic_username" > "$FILE"
     echo "thycotic_password=$thycotic_password" >> "$FILE"
     echo "thycotic_sub_domaine=$thycotic_sub_domaine" >> "$FILE"
@@ -33,7 +28,6 @@ if [ ! -f "$FILE" ] || [ "$1" = "login" ]; then
     exit 0
 fi
 
-# Read values from the file
 thycotic_username=$(grep 'thycotic_username' "$FILE" | cut -d '=' -f 2)
 thycotic_password=$(grep 'thycotic_password' "$FILE" | cut -d '=' -f 2)
 thycotic_sub_domaine=$(grep 'thycotic_sub_domaine' "$FILE" | cut -d '=' -f 2)
@@ -61,21 +55,15 @@ is_integer() {
     [[ $1 =~ ^[0-9]+$ ]]
 }
 
-# Function to check if a string is a valid IP address
 is_valid_ip() {
   ip="$1"
-  # Check if IP address is present
   if [[ -z "$ip" ]]; then
     false
   fi
 
-  # Regex for valid IPv4 address
   re='^([0-9]{1,3}\.){3}[0-9]{1,3}$'
-  # Check if IP matches the regex
   if [[ "$ip" =~ $re ]]; then
-    # Split IP into octets
     IFS='.' read -r -a octets <<< "$ip"
-    # Validate each octet (range 0-255)
     for octet in "${octets[@]}"; do
       if [[ $octet -lt 0 || $octet -gt 255 ]]; then
         false
@@ -87,37 +75,30 @@ is_valid_ip() {
   fi
 }
 
-# Function to check if a string is a valid domain name
 is_valid_domain() {
   domain="$1"
-  # Check if domain is present
   if [[ -z "$domain" ]]; then
      false
   fi
 
-  # Regex for valid domain name
   re='^([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}$'
-  # Check if domain matches the regex
   if [[ "$domain" =~ $re ]]; then
-    # shellcheck disable=SC2152
      true
   else
      false
   fi
 }
 
-# Check if $1 is an integer
 if ! is_integer "$1"; then
     echo -e "${RED}Erreur: Le secret id doit être un entier${NC}"
     exit 1
 fi
 
-# Check if $2 is a valid IP address or domain name
 if ! (is_valid_ip "$2" || is_valid_domain "$2"); then
     echo "Le ip ou domaine du serveur n'est pas valide"
     exit 1
 fi
-# Configuration
+
 API_URL="https://${thycotic_sub_domaine}.secretservercloud.eu/oauth2/token"
 SECRET_SERVER_URL="https://${thycotic_sub_domaine}.secretservercloud.eu/api/v1/secrets/sshproxy"
 unset SECRET_ID
@@ -125,7 +106,6 @@ unset TARGET_SERVER_IP
 SECRET_ID="$1"
 TARGET_SERVER_IP="$2"
 
-# Obtenir un jeton d'accès
 echo -e "${YELLOW}Start login ...${NC}"
 unset RESPONSE
 RESPONSE=$(curl -s --location $API_URL \
@@ -143,7 +123,7 @@ if [ -z "$ACCESS_TOKEN" ] || [ "${ACCESS_TOKEN}" = "null" ]; then
   else
      echo -e "${GREEN}Login to thycotic success ${NC}"
 fi
-echo -e "${YELLOW}Start get access ...${NC}"
+echo -e "${YELLOW}Start getting access ...${NC}"
 unset RESPONSE_PROXY
 RESPONSE_PROXY=$(curl -s --location $SECRET_SERVER_URL \
                  --header 'Content-Type: application/json' \
