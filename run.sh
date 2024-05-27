755e4426d9b6efd96ebcb519bc95f21c6bb4cfc2
Pape Samba NDOUR <papesambandour@hotmail.com>
2024-05-27 17:42:23 +0000

    init repo

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 echo "alias thycotic=$SCRIPT_DIR/generate-ssh-proxy.sh" >> ~/.zshrc
+# shellcheck disable=SC1090
+source ~/.zshrc
+echo -e "${GREEN}thycotic is installed successfully${NC}"
