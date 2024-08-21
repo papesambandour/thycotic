44ae16567618b1d5c35e91cf31d527e2189978f9
papesambandour <papesambandour@hotmail.com>
2024-08-21 22:53:02 +0000

    Verions 2.0 - [x] Update script - [x] Add server - [x] Remote secret id on command ssh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-echo "alias thycotic=$SCRIPT_DIR/generate-ssh-proxy.sh" >> ~/.zshrc
+#echo "alias thycotic=$SCRIPT_DIR/generate-ssh-proxy.sh" >> ~/.zshrc
+echo "alias thycotic=$SCRIPT_DIR/ssh-proxy.sh" >> ~/.zshrc
 # shellcheck disable=SC1090
 source ~/.zshrc
 echo -e "${GREEN}thycotic is installed successfully${NC}"
